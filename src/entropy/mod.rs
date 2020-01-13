159280c66e217809d7aa7bfee35b9336a4aec9c9
Sergey <xxsmotur@gmail.com>
2020-01-13 22:40:56 +0500

    collecting of elements

--- src/entropy/mod.rs
+++ src/entropy/mod.rs
@@ -5,3 +5,19 @@ pub trait Entropy {
     fn compute(i1:Input, i2:Input) -> Result<String, Error>
 }
 
+#[derive(Debug, PartialEq)]
+pub struct Conditional {
+    i1: Input
+}
+
+impl Conditional for Entropy {
+    fn new(&self, i1:Input) -> self {
+        self.i1 = i1
+        self
+    }
+
+    fn compute(&self) -> Result<String, Error> {
+        self.i1.iter().map(|&x| x + 1).collect::<Vec<_>>();
+    }
+}
+
