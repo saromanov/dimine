06224fe3b36c8dbb977c3a4bd828b3512e0adce0
Sergey <xxsmotur@gmail.com>
2020-01-14 07:24:15 +0500

    replacing with generic types

--- src/entropy/mod.rs
+++ src/entropy/mod.rs
@@ -1,16 +1,16 @@
 
 pub type Input = Vec<i32>;
 
-pub trait Entropy {
-    fn compute(i1:Input, i2:Input) -> Result<String, Error>
+pub trait Entropy<T: Ord> {
+    fn compute(i1:T, i2:T) -> Result<String, Error>
 }
 
 #[derive(Debug, PartialEq)]
-pub struct Conditional {
-    i1: Input
+pub struct Conditional<T: Ord> {
+    i1: T
 }
 
-impl Conditional for Entropy {
+impl Conditional<T: Ord> for Entropy {
     fn new(&self, i1:Input) -> self {
         self.i1 = i1
         self
