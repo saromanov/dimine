992eb863570d1d31c9c5ce925d101f88eb209199
Sergey <xxsmotur@gmail.com>
2020-01-14 07:28:42 +0500

    Fix typos

--- src/entropy/mod.rs
+++ src/entropy/mod.rs
@@ -2,7 +2,7 @@
 pub type Input = Vec<i32>;
 
 pub trait Entropy<T: Ord> {
-    fn compute(i1:T, i2:T) -> Result<String, Error>
+    fn compute(i1:T, i2:T) -> Result<String, String>;
 }
 
 #[derive(Debug, PartialEq)]
@@ -10,14 +10,14 @@ pub struct Conditional<T: Ord> {
     i1: T
 }
 
-impl Conditional<T: Ord> for Entropy {
-    fn new(&self, i1:Input) -> self {
+impl <T:Ord> Entropy<T> for Conditional<T> {
+    fn new(&self, i1:T) -> Self {
         self.i1 = i1
-        self
     }
 
-    fn compute(&self) -> Result<String, Error> {
+    fn compute(&self) -> Result<String, String> {
         self.i1.iter().map(|&x| x + 1).collect::<Vec<_>>();
+        Ok("aaa")
     }
 }
 
